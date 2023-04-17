679e2d009c50a269c62e5c87c40745b1083ccc99
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-17 11:55:01 -0600

    add total_inventory, not working

--- lib/market.rb
+++ lib/market.rb
@@ -16,4 +16,15 @@ class Market
   def vendors_that_sell(item)
     vendors.select { |vendor| vendor.inventory.include?(item) }
   end
+
+  def sorted_item_list
+    vendors.flat_map { |vendor| vendor.inventory.keys.map { |item| item.name } }.sort.uniq
+  end
+
+  def total_inventory
+    outer = Hash.new(0)
+    vendors.each do |vendor|
+    items = vendor.inventory.keys.each { |item, amount| outer[item] = {"Quantity" => amount, "Vendors" => vendor} }  
+    end
+  end
 end
