4beb405a6b29f277711d73d3ec6071a02188b7a8
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-17 09:29:34 -0600

    test: Add test for check_stock to see inventory amount added

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -12,13 +12,16 @@ RSpec.describe Vendor do
       expect(vendor.inventory).to eq({})
     end
 
-    it "can check stock for an item, can add item to inventory" do
+    it "can check stock for an item, can add item to inventory, checks stock again" do
       vendor = Vendor.new("Rocky Mountain Fresh")
       item1 = Item.new({name: 'Peach', price: "$0.75"})
       expect(vendor.check_stock(item1)).to eq(0)
       vendor.stock(item1, 30)
       expect(vendor.inventory).to eq({item1 => 30})
       expect(vendor.check_stock(item1)).to eq(30)
+      vendor.stock(item1, 25)
+      expect(vendor.check_stock(item1)).to eq(55)
+
     end
   end
 end
@@ -34,8 +37,6 @@ end
 
 
 
-# vendor.check_stock(item1)
-# #=> 30
 
 # vendor.stock(item1, 25)
 
