e70e80cdff79445564138d4eb86632288c70d730
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-17 09:27:40 -0600

    test: Add tests for vendor inventory, check stock, stock methods

---
+++ spec/vendor_spec.rb
@@ -0,0 +1,48 @@
+require 'rspec'
+require './lib/item'
+require './lib/vendor'
+
+
+RSpec.describe Vendor do
+  describe "vendor" do
+    it "exists, has name, starts with an empty inventory" do
+      vendor = Vendor.new("Rocky Mountain Fresh")
+      expect(vendor).to be_a(Vendor)
+      expect(vendor.name).to eq("Rocky Mountain Fresh")
+      expect(vendor.inventory).to eq({})
+    end
+
+    it "can check stock for an item, can add item to inventory" do
+      vendor = Vendor.new("Rocky Mountain Fresh")
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      expect(vendor.check_stock(item1)).to eq(0)
+      vendor.stock(item1, 30)
+      expect(vendor.inventory).to eq({item1 => 30})
+      expect(vendor.check_stock(item1)).to eq(30)
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+# vendor.check_stock(item1)
+# #=> 30
+
+# vendor.stock(item1, 25)
+
+# vendor.check_stock(item1)
+# #=> 55
+
+# vendor.stock(item2, 12)
+
+# vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
